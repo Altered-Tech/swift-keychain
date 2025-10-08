cc4f6025daaf921ddf60b891b21cea234e38df41
Michael Einreinhof <61751327+alteredtech@users.noreply.github.com>
2025-10-07 20:02:15 -0600

    Change KeychainClient protocol to public

--- Sources/Keychain/KeychainClient.swift
+++ Sources/Keychain/KeychainClient.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-protocol KeychainClient {
-    func add(_ query: CFDictionary) -> OSStatus
-    func update(_ query: CFDictionary, _ attributes: CFDictionary) -> OSStatus
-    func copyMatching(_ query: CFDictionary, _ result: UnsafeMutablePointer<AnyObject?>) -> OSStatus
-    func delete(_ query: CFDictionary) -> OSStatus
+public protocol KeychainClient {
+    public func add(_ query: CFDictionary) -> OSStatus
+    public func update(_ query: CFDictionary, _ attributes: CFDictionary) -> OSStatus
+    public func copyMatching(_ query: CFDictionary, _ result: UnsafeMutablePointer<AnyObject?>) -> OSStatus
+    public func delete(_ query: CFDictionary) -> OSStatus
 }
